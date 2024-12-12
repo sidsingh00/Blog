e88931d622d0db057e870986b3e4ac4787b9d8d6
sidharth singh <sidharthsingh@sidharths-MacBook-Air.local>
2024-12-13 03:13:45 +0530

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,48 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
+import {sign} from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string;
+    JWT_SECRET: string;
+  }
+}>()
 
-app.post ('/api/v1/user/signup', (c) => {
+app.post ('/api/v1/user/signup', async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl : c.env.DATABASE_URL,
+  }).$extends(withAccelerate()); 
+
+
+  const body = await c.req.json();
+
+  try{
+    const user = await prisma.user.create({
+      data:{
+        username : body.username,
+        password : body.password,
+        name : body.name,
+      }
+    })
+    const jwt = await sign({id : user.id},c.env.JWT_SECRET);
+
+    return c.json({
+      message : "user is created"
+    })
+  }
+  catch(e){
+    c.status(403)
+    console.log(e);
+    return c.json({
+      message : "user can't be created"
+    })
+  }
+
+
+
   return c.text('Hello Hono!')
 })
 
